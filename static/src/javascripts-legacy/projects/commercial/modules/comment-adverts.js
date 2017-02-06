31fcb7fe6b4b344d7e909e9d0054a25e949eed16
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-02-06 16:08:39 +0000

    Simplify comment adverts

--- static/src/javascripts-legacy/projects/commercial/modules/comment-adverts.js
+++ static/src/javascripts-legacy/projects/commercial/modules/comment-adverts.js
@@ -1,65 +1,45 @@
 define([
-    'Promise',
     'common/utils/$',
     'common/utils/config',
-    'common/utils/detect',
     'common/utils/mediator',
-    'common/utils/fastdom-idle',
-    'common/modules/identity/api',
-    'common/modules/experiments/ab',
+    'common/utils/fastdom-promise',
     'common/modules/commercial/dfp/add-slot',
     'common/modules/commercial/commercial-features',
-    'common/modules/commercial/dfp/create-slot',
-    'lodash/objects/defaults'
+    'common/modules/commercial/dfp/create-slot'
 ], function (
-    Promise,
     $,
     config,
-    detect,
     mediator,
-    idleFastdom,
-    identityApi,
-    ab,
+    fastdom,
     addSlot,
     commercialFeatures,
-    createSlot,
-    defaults
+    createSlot
 ) {
-    return function (options) {
-        var opts = defaults(
-                options || {},
-                {
-                    adSlotContainerSelector: '.js-discussion__ad-slot',
-                    commentMainColumn: '.content__main-column'
-                }
-            ),
-            $adSlotContainer,
-            $commentMainColumn;
-
-        $adSlotContainer = $(opts.adSlotContainerSelector);
-        $commentMainColumn = $(opts.commentMainColumn, '.js-comments');
+    return function () {
+        var $adSlotContainer = $('.js-discussion__ad-slot');
 
         if (!commercialFeatures.commentAdverts || !$adSlotContainer.length) {
             return false;
         }
 
         mediator.once('modules:comments:renderComments:rendered', function () {
-            idleFastdom.read(function () {
+            var $commentMainColumn = $('.js-comments .content__main-column');
+
+            fastdom.read(function () {
+                return $commentMainColumn.dim().height;
+            })
+            .then(function(mainColHeight) {
                 //if comments container is lower than 280px
-                if ($commentMainColumn.dim().height < 280) {
+                if (mainColHeight < 280) {
                     return false;
                 }
 
-                idleFastdom.write(function () {
-                    var adSlot;
+                var adSlot = createSlot('container', { classes: 'mpu-banner-ad', name: 'comments' });
+                var classNames = 'discussion__ad-wrapper' +
+                    (!config.page.isLiveBlog && !config.page.isMinuteArticle ? ' discussion__ad-wrapper-wider' : '')
 
-                    $commentMainColumn.addClass('discussion__ad-wrapper');
-
-                    if (!config.page.isLiveBlog && !config.page.isMinuteArticle) {
-                        $commentMainColumn.addClass('discussion__ad-wrapper-wider');
-                    }
-
-                    adSlot = createSlot('container', { classes: 'mpu-banner-ad', name: 'comments' });
+                fastdom.write(function () {
+                    $commentMainColumn.addClass(classNames);
                     $adSlotContainer.append(adSlot);
                     addSlot(adSlot);
                 });
